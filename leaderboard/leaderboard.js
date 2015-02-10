f1fc0ec68afa3264f358e76f2a4f5681b2b0b7c1
Raghavendra Ugare <raghu.ugare@gmail.com>
2015-02-10 14:39:17 +0530

    Creating a helper function (deprecated way)...

--- leaderboard/leaderboard.js
+++ leaderboard/leaderboard.js
@@ -2,6 +2,11 @@ PlayerList = new Mongo.Collection('players');
 
 if(Meteor.isClient) {
     console.log("Hello from the Client! :)");
+
+    Template.leaderboard.player = function() {
+        return "From player function()...";
+    }
+    
 }
 
 if(Meteor.isServer) {
