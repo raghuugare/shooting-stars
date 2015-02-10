0a90f0a94cad031213b5f7827e74f917f47446eb
Raghavendra Ugare <raghu.ugare@gmail.com>
2015-02-10 14:26:25 +0530

    Client vs. Server difference

--- leaderboard/leaderboard.js
+++ leaderboard/leaderboard.js
@@ -1,2 +1,9 @@
 PlayerList = new Mongo.Collection('players');
 
+if(Meteor.isClient) {
+    console.log("Hello from the Client! :)");
+}
+
+if(Meteor.isServer) {
+    console.log("Hello from the Server! :)");
+}
